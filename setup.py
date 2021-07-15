54f09ec0849a4b686cd8d9bca5ebe06e17f3d5f2
VenierGiacomo <giacomo.venier@gmail.com>
2021-07-15 11:27:08 +0200

    Send instant and future push notifications

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Apple :: macOs :: macOs Big Sur 11.2.3',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='django-onesignal',
+  version='1.0.0',
+  description='An interface to send push notification (monile and web) from Django',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Giacomo Venie',
+  author_email='giacomo.venier@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='django, onesignal', 
+  packages=find_packages(),
+  install_requires=['requests, json, datetime'] 
+)
